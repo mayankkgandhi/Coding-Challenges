a6f2dbbbb29a8c71f0555656f0dea7edbf6a7a8b
Mayank Gandhi <mayank.k.gandhi@gmail.com>
2017-03-25 00:05:27 +0530

    Update 10.py

--- 30 Days Challenge/10.py
+++ 30 Days Challenge/10.py
@@ -1,2 +1,14 @@
+#1) Basic sol
+def factorial(n):
+    if n is 1:
+        return n
+    else:
+        return (n * factorial(n-1))
+if __name__ == '__main__':
+    
+    N=int(raw_input())        
+    print(factorial(N))
+    
+# Advance sol
 factorial = lambda x : 1 if x<=1 else x*factorial(x-1)
 print(factorial(int(raw_input()))
