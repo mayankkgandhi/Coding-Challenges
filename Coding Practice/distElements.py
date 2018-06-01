6c99e74d0996a1aebe5efadb00cce4d9458182fb
Mayank Gandhi <mayank.k.gandhi@gmail.com>
2018-06-01 11:41:43 +0530

    Update distElements.py

--- Coding Practice/distElements.py
+++ Coding Practice/distElements.py
@@ -4,7 +4,9 @@ def findDistinct(arr,l,k):
     m=dict()
     dist_count = 0 
     for i in range (0,k):
+
         if arr[i] in m.keys():
+            # print m[arr[i]]
             m[arr[i]] +=1
         else:
             m[arr[i]] = 1
@@ -15,14 +17,12 @@ def findDistinct(arr,l,k):
     # print k
     for i in range(k,l):
         if m[arr[i-k]] == 1:
-            # m[arr[i-k]] -=1
-            # dist_count -= 1
-            m[arr[i-k]] - 1
+            dist_count -= 1
+            m.pop(arr[i-k])
         else:
             m[arr[i-k]] -= 1
-        
-        print m
-        # print i
+        # print m
+        # print arr[i]
         if arr[i] in m.keys():
             m[arr[i]] +=1
         else:
@@ -33,7 +33,6 @@ def findDistinct(arr,l,k):
         print " Dist Count =",dist_count
 
 
-
 A=[1, 2, 1, 3, 4, 2, 3]
-
+print A
 findDistinct(A,len(A),4)
